632ce8c62bf2a3e40cf0dc0a94954ce208325583
Malte Plath <m.c.plath@web.de>
2021-01-04 22:28:56 +0100

    find_path is not yet working

--- paths.py
+++ paths.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 def point_is_on_line(point, line):
     """Determine if given point lies on the given line
 
@@ -23,11 +26,65 @@ def lines_with_point(lines, point):
 def split_line_at_point(line, point):
     if not point_is_on_line(point, line):
         raise ValueError(f"Point {point} is not on line {line}")
-    if point in line:
+    # Make sure we get the directions of the resulting line(s) correct
+    if point == line[0]:
         return [line]
+    elif point == line[1]:
+        return [(line[1], line[0])]
     else:
-        return [(line[0], point), (point, line[1])]
+        return [(point, line[0]), (point, line[1])]
 
 
-def find_path(all_paths, closing):
-    pass
+def orientation(line_from, line_to):
+    """Coming along `line_from`, which way do we have to turn?
+
+    Left turn:
+
+             ^
+             |  line_from
+             |
+      +----->+
+      line_to
+
+    -1: turn right
+     0: straight on
+    +1: turn left
+    """
+    if line_from[1] not in line_to:
+        raise ValueError(f"Lines {line_from} and {line_to} do not connect")
+    f0 = pygame.Vector2(line_from[0])
+    f1 = pygame.Vector2(line_from[1])
+    t0 = pygame.Vector2(line_to[0])
+    t1 = pygame.Vector2(line_to[1])
+    turn = (f1 - f0).cross(t1 - t0)
+    # The sign is inverted because screen coordinates are upside down
+    # (Y axis increases downwards, ie. we are calculating with the mirror image)
+    if turn < 0:
+        return 1
+    elif turn > 0:
+        return -1
+    else:
+        return 0
+
+
+def find_path(all_paths, open_loop):
+    # start with final line segment in open loop
+    finish_point = open_loop[0][0]
+    finish_line = (open_loop[0][1], open_loop[0][0])
+    starting_line = open_loop[-1]
+    starting_point = starting_line[1]
+    lines_at_start = []
+    lwps = lines_with_point(all_paths, starting_point)
+    for full_line in lwps:
+        splits = split_line_at_point(full_line, starting_point)
+        for line in splits:
+            # walk in counter-clockwise direction
+            if orientation(starting_line, line) < 0:
+                lines_at_start.append(line)
+    lwps = lines_with_point(lines_at_start, finish_point)
+    for full_line in lwps:
+        splits = split_line_at_point(full_line, finish_point)
+        for line in splits:
+            if orientation(finish_line, line) > 0:
+                lines_at_start.append(line)
+    return lines_at_start, finish_line
--- test_close_area.py
+++ test_close_area.py
@@ -1,6 +1,7 @@
+import pygame
 import pytest
 
-from paths import lines_with_point, point_is_on_line, split_line_at_point
+from paths import lines_with_point, point_is_on_line, split_line_at_point, orientation, find_path
 
 
 @pytest.fixture(scope='module')
@@ -10,14 +11,8 @@ def boundary_rectangular():
         (40, 50), (40, 0),
         (0, 0),
     ]
+    yield list(zip(b, b[1:]))
 
-
-# def test_find_path(boundary_rectangular):
-#     stix = [(0, 20), (40, 20)]
-#     closed = [
-#         (40, 20), (40, 0), (0, 0), (0, 20), (40, 20),
-#     ]
-#     assert find_path(boundary_rectangular, stix) == closed
 
 def test_point_is_on_line_true1():
     the_point = (0, 20)
@@ -91,11 +86,22 @@ def test_split_line_at_point1():
 
 
 def test_split_line_at_point2():
+    """When the line meets the point, the result is always delivered with the
+    given point as the first point"""
+    the_point = (0, 0)
+    the_line = ((50, 0), (0, 0))
+    result = split_line_at_point(the_line, the_point)
+    assert len(result) == 1
+    assert ((0, 0), (50, 0)) in result
+
+
+def test_split_line_at_point3():
+    """When line is split, both segments start with the given point"""
     the_point = (30, 0)
     the_line = ((50, 0), (0, 0))
     result = split_line_at_point(the_line, the_point)
     assert len(result) == 2
-    assert ((50, 0), (30, 0)) in result
+    assert ((30, 0), (50, 0)) in result
     assert ((30, 0), (0, 0)) in result
 
 
@@ -105,3 +111,77 @@ def test_split_line_at_point_value_error():
     with pytest.raises(ValueError) as exc:
         result = split_line_at_point(the_line, the_point)
     assert "is not on line" in str(exc)
+
+
+def test_orientation_leftturn1():
+    line_from = ((0, 0), (0, 10))
+    line_to = ((0, 10), (10, 10))
+    assert orientation(line_from, line_to) == 1, "Should be left turn"
+
+
+def test_orientation_leftturn2():
+    line_from = ((10, 0), (0, 0))
+    line_to = ((0, 0), (0, 5))
+    assert orientation(line_from, line_to) == 1, "Should be left turn"
+
+
+def test_orientation_rightturn1():
+    line_from = ((0, 0), (10, 0))
+    line_to = ((10, 0), (10, 10))
+    assert orientation(line_from, line_to) == -1, "Should be right turn"
+
+
+def test_orientation_raises_value_error():
+    line_from = ((10, 10), (10, 0))
+    line_to = ((0, 10), (0, 0))
+    with pytest.raises(ValueError) as exc:
+        orientation(line_from, line_to)
+    assert "do not connect" in str(exc)
+
+
+def test_orientation4():
+    line_from = ((10, 10), (10, 5))
+    line_to = ((10, 5), (3, 5))
+    assert orientation(line_from, line_to) == 1, "Should be left turn"
+
+
+def test_cross():
+    """The sign is inverted because screen coordinates are upside down
+    (Y axis increases downwards, ie. we are calculating with the mirror image)"""
+    v1 = pygame.Vector2(0, 1)
+    v2 = pygame.Vector2(1, 0)
+    assert v1.cross(v2) == -1
+
+
+def test_find_path_prequel(boundary_rectangular):
+    stix_as_lines = [((0, 20), (20, 20)),
+                     ((20, 20), (20, 30)),
+                     ((20, 30), (0, 30))
+                     ]
+    closed = [
+        ((0, 20), (20, 20)),
+        ((20, 20), (20, 30)),
+        ((20, 30), (0, 30)),
+        ((0, 30), (0, 20))
+    ]
+    starting_line = stix_as_lines[-1]
+    first_line = lines_with_point(boundary_rectangular, starting_line[1])[0]
+    assert first_line == ((0, 0), (0, 50))
+    splits = split_line_at_point(first_line, starting_line[1])
+    assert splits == [((0, 30), (0, 0)), ((0, 30), (0, 50))]
+    assert (orientation(starting_line, splits[0]) > 0
+            or orientation(starting_line, splits[1]) > 0)
+
+
+def test_find_path(boundary_rectangular):
+    stix_as_lines = [((0, 20), (20, 20)),
+                     ((20, 20), (20, 30)),
+                     ((20, 30), (0, 30))
+                     ]
+    closed = [
+        ((0, 20), (20, 20)),
+        ((20, 20), (20, 30)),
+        ((20, 30), (0, 30)),
+        ((0, 30), (0, 20))
+    ]
+    assert find_path(boundary_rectangular, stix_as_lines) == closed
